51891d8e2da04bfcadf2c27c8a00d7b4f285513b
Matthias <mail@matthiasbock.net>
2012-11-01 10:38:28 +0100

    new httpclient

--- wieistmeineip.py
+++ wieistmeineip.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 
-from robot import Robot
+from httpclient import HttpClient
 from htmlparser import between
 
 def External_IP():
-	r = Robot()
+	r = HttpClient()
 	r.GET("http://www.wieistmeineip.de/")
 	ip = between(r.Page, 'CopyToClipboard(\'', '\'')
 	if ip == '':
