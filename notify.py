5750ffff1601d7b35780dfba699eb6d3cc97a552
Matthias <mail@matthiasbock.net>
2012-11-01 10:40:16 +0100

    report_hostup.py

--- notify.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: iso-8859-15 -*-
-
-from ping import ping
-from mailer import MailTransport, Email
-
-if ping('kanzler-pc'):
-	Email(From='Kafka <carpediemd27@web.de>', To='matthias.bock@hu-berlin.de', Subject='Der Kanzler ist da').send( MailTransport(Account='carpediemd27@web.de') )
-
