bf18ee55a51f531bc8d3e570ffd4c23d51d01638
Matthias <mail@matthiasbock.net>
2013-06-09 15:56:17 +0200

    rsync script

---
+++ rsync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/Backups/Luecke/git/
+rsync -ar --delete -i /media/Luecke/C/Users/Lücke/AppData/Local/IM/* /home/Backups/Luecke/git/IM/ &> /home/Backups/Luecke/IM.log
+git add .
+git commit -am "$(echo -n $(date)): $(echo -n $(cat /etc/hostname)) rsync'ed C:/Users/Luecke/AppData/Local/IM/ on LUECKE-PC (192.168.3.114) with IM/"
+
