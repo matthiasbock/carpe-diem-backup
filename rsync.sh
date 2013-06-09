f2aa5b3e6ca5bfafe7472b13ea59c3b28b962649
Matthias <mail@matthiasbock.net>
2013-06-09 23:37:41 +0200

    mount

--- rsync.sh
+++ rsync.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+date=$(echo -n $(date))
+
+umount -fl /media/Luecke/C
+sleep 5
+mount /media/Luecke/C
+
 cd /home/Backups/Luecke/git/
 rsync -ar --delete -i /media/Luecke/C/Users/Lücke/AppData/Local/IM/* /home/Backups/Luecke/git/IM/ &> /home/Backups/Luecke/IM.log
 git add .
-git commit -am "$(echo -n $(date)): $(echo -n $(cat /etc/hostname)) rsync'ed C:/Users/Luecke/AppData/Local/IM/ on LUECKE-PC (192.168.3.114) with IM/"
+git commit -am "$date: $(echo -n $(cat /etc/hostname)) rsync'ed C:/Users/Luecke/AppData/Local/IM/ on LUECKE-PC (192.168.3.114) with IM/"
 
