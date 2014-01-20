539b0fb0918e77b2f762052c6a60675205e3c2ad
Matthias <mail@matthiasbock.net>
2014-01-20 10:59:48 +0100

    ethers updated

--- ethers.py
+++ ethers.py
@@ -12,6 +12,7 @@ from utils import between
 
 def getIP():
 	output = Popen(['ifconfig'], stdout=PIPE).communicate()[0]
+	print output
 	return between( between(output, 'eth0', '\n\n'), 	'inet addr:', ' ' )
 
 def ether2ip(find_ether):
@@ -19,7 +20,7 @@ def ether2ip(find_ether):
 	netmask = '.'.join( getIP().split('.')[0:3] )
 	reply = Popen(split('nmap -sP '+netmask+'.1-245'), stdout=PIPE).communicate()[0]
 	for line in reply.split('\n'):
-		if 'Host ' in line and ' up' in line:
+		if ' scan report ' in line:
 			ip = between(line, '(', ')')
 		elif 'MAC Address:' in line:
 			ether = between(line, ': ', ' ')
@@ -41,16 +42,20 @@ def update_hosts(ip, smbname):
 	open('/etc/hosts', 'w').write(hosts)
 	
 ethers = open('/etc/ethers').read()
+print ethers
 
 for line in ethers.split('\n'):
-	if len(line) > 6*2+5+1 and '\t' in line:
-		s = line.split('\t')
-		ether = s[0].strip()
-		smbname = ' '.join(s[1:])
-		ip = ether2ip(ether)
-		if ip is not None:
-			update_hosts(ip, smbname)
-			print smbname+' is at '+ip
-		else:
-			print 'could not resolve '+ether+' ('+smbname+')'
+	if len(line.strip()) > 0:
+		if line[0] != '#':
+			s = line.split('\t')
+			ether = s[0].strip()
+			smbname = ' '.join(s[1:])
+			print ether
+			print smbname
+			ip = ether2ip(ether)
+			if ip is not None:
+				update_hosts(ip, smbname)
+				print smbname+' is at '+ip
+			else:
+				print 'could not resolve '+ether+' ('+smbname+')'
 
